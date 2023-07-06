7fa3868b251afa2f2d9a17072a867c90bffaa107
Roaasameh01 <roaas685@gmail.com>
2023-07-05 19:56:33 -0700

    a

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,7 +8,7 @@
  */
 char *move_past_star(char *s2)
 {
-	if (s2 == '*')
+	if (*s2 == '*')
 		return (move_past_star(s2 + 1));
 	else
 		return (s2);
@@ -28,8 +28,8 @@ int inception(char *s1, char *s2)
 	if (*s1 == 0)
 		return (0);
 	if (*s1 == *s2)
-		ret += wildcmp(s1 + 1, s2 + 1);
-	ret += inception(s1 + 1, s2);
+		ret += wildcmp(s1 + 1, s2 + 1)
+			ret += inception(s1 + 1, s2);
 	return (ret);
 }
 
@@ -44,17 +44,17 @@ int wildcmp(char *s1, char *s2)
 {
 	int ret = 0;
 
-	if (!s1 && *s2 == '*' && !*move_past_star(s2))
+	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
 		return (1);
 	if (*s1 == *s2)
 	{
 		if (!*s1)
 			return (1);
-		return (wildcmp(s1 + 1, s2 == '*' ? s2 : s2 + 1));
+		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
 	}
 	if (!*s1 || !s2)
 		return (0);
-	if (s2 == '*')
+	if (*s2 == '*')
 	{
 		s2 = move_past_star(s2);
 		if (!*s2)
